d25ca93e7dea41d555375b04c84a1e82b5f1b7cf
mouaz-ma <mouaz-m@Hotmail.com>
2022-04-27 22:58:06 +0300

    changed landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,15 @@
 <template>
   <div>
-    <NavOne />
-    <BannerOne />
-    <AboutTwo />
-    <CourseOne />
-    <VideoTwo />
-    <CountdownOne />
-    <CourseCategory />
-    <CallToActionThree />
-    <ClientsLogoTwo />
-    <BlogCarousel />
-    <CallToActionFour />
-    <Subscribe />
-    <Footer />
+     <CallToActionTwo />
   </div>
 </template>
 
 <script>
-  import NavOne from "../components/NavOne";
-  import BannerOne from "../components/BannerOne";
-  import Footer from "../components/Footer";
-  import AboutTwo from "../components/AboutTwo";
-  import CourseOne from "../components/CourseOne";
-  import VideoTwo from "../components/VideoTwo";
-  import CountdownOne from "../components/CountdownOne";
-  import CourseCategory from "../components/CourseCategory";
-  import CallToActionThree from "../components/CallToActionThree";
-  import ClientsLogoTwo from "../components/ClientsLogoTwo";
-  import BlogCarousel from "../components/BlogCarousel";
-  import CallToActionFour from "../components/CallToActionFour";
-  import Subscribe from "../components/Subscribe";
+  import CallToActionTwo from "../components/CallToActionTwo";
 
   export default {
     components: {
-      Subscribe,
-      CallToActionFour,
-      BlogCarousel,
-      ClientsLogoTwo,
-      CallToActionThree,
-      CourseCategory,
-      CountdownOne,
-      VideoTwo,
-      CourseOne,
-      NavOne,
-      BannerOne,
-      AboutTwo,
-      Footer,
+    CallToActionTwo
     }
   }
 </script>
