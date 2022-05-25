0117bfb25af0f6748640c1c099454ba8a1a2faab
mouaz-ma <mouaz-m@Hotmail.com>
2022-05-25 16:43:56 +0300

    working on classroom

--- components/uploadClassForm.vue
+++ components/uploadClassForm.vue
@@ -20,6 +20,13 @@
                       <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                           <input type="text" v-model="zoomLink" placeholder="zoomLink">
                       </div><!-- /.col-lg-6 -->
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <label for="example-datepicker">Class Room time</label>
+                          <b-form-datepicker id="example-datepicker" today-button reset-button v-model="date" class="mb-2"></b-form-datepicker>
+                          <p>date: '{{ date }}'</p>
+                          <b-form-timepicker v-model="time" locale="en" now-button reset-button></b-form-timepicker>
+                          <div class="mt-2">time: '{{ time }}'</div>
+                      </div>
                       <!-- image upload -->
                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                 <b-form-file
@@ -71,6 +78,8 @@ export default {
       description: '',
       zoomLink: '',
       type: '',
+      date: '',
+      time: '',
     }
   },
   methods: {
@@ -84,6 +93,8 @@ export default {
         data.append("userID", this.$auth.$state.user._id);
         data.append("image", this.image);
         data.append("zoomLink", this.zoomLink);
+        data.append("classDate", this.date);
+        data.append("classTime", this.time);
         if (this.image) {
           if (this.image.type === "image/jpeg") {
             let response = await this.$axios.post('/api/classRoom/new', data);
