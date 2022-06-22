c39f2cb4c5cc2d3d6156ed18ba0fd49ebb70c4f6
Nasr Galal <nasrbue@gmail.com>
2022-06-22 21:29:33 +0200

    feat: add profile page

--- pages/users/profile/_id.vue
+++ pages/users/profile/_id.vue
@@ -1,24 +1,120 @@
 <template>
-<h1>{{$route.params.id}}</h1>
+  <div class="container emp-profile">
+    <form method="post">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="profile-img">
+            <img
+              src="https://www.w3schools.com/w3images/avatar5.png"
+              alt=""
+            >
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="profile-head">
+            <h5>
+              {{ userData.username }}
+            </h5>
+            <h6>
+              {{ userData.email }}
+            </h6>
+            <p class="proile-rating">
+              Email Status:
+              <span :class="userData.isVerified ? 'text-success' : 'text-danger'">
+                {{ userData.isVerified ? 'Verified' : 'Unverified' }}
+              </span>
+            </p>
+            <ul
+              id="myTab"
+              class="nav nav-tabs"
+              role="tablist"
+            >
+              <li class="nav-item">
+                <a
+                  id="home-tab"
+                  class="nav-link active"
+                  data-toggle="tab"
+                  href="#home"
+                  role="tab"
+                  aria-controls="home"
+                  aria-selected="true"
+                >About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-4">
+          <!-- <div class="profile-work">
+            <p>WORK LINK</p>
+            <a href="">Website Link</a><br>
+            <a href="">Bootsnipp Profile</a><br>
+            <a href="">Bootply Profile</a>
+            <p>SKILLS</p>
+            <a href="">Web Designer</a><br>
+            <a href="">Web Developer</a><br>
+            <a href="">WordPress</a><br>
+            <a href="">WooCommerce</a><br>
+            <a href="">PHP, .Net</a><br>
+          </div> -->
+        </div>
+        <div class="col-md-8">
+          <div
+            id="myTabContent"
+            class="tab-content profile-tab"
+          >
+            <div
+              id="home"
+              class="tab-pane fade show active"
+              role="tabpanel"
+              aria-labelledby="home-tab"
+            >
+              <div class="row">
+                <div class="col-md-6">
+                  <label>User Id</label>
+                </div>
+                <div class="col-md-6">
+                  <p>{{ userData._id }}</p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6">
+                  <label>Name</label>
+                </div>
+                <div class="col-md-6">
+                  <p>{{ userData.username }}</p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6">
+                  <label>Email</label>
+                </div>
+                <div class="col-md-6">
+                  <p>{{ userData.email }}</p>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-6">
+                  <label>Unique String</label>
+                </div>
+                <div class="col-md-6">
+                  <p>{{ userData.uniqueString }}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
 <script>
 
 export default {
-
-    data() {
-        return{
-            name:"",
-            email:"",
-            verifyAlertMessage: '',
-            searchQuery: '',
-            userData: null,
-        }
-    },
-    methods: {
-    },
-      async asyncData({
-      $axios, route
-    }) {
+    async asyncData({ $axios, route }) {
       try {
         const userCall = $axios.get('/api/users/userInfo/'+route.params.id)
         const userPromise = await Promise.resolve(userCall)
@@ -31,18 +127,115 @@ export default {
         console.log(err)
       }
     },
+
+    data() {
+      return{
+        name:"",
+        email:"",
+        verifyAlertMessage: '',
+        searchQuery: '',
+        userData: null,
+      }
+    },
+
+    methods: {},
 }
 </script>
 
 <style scoped>
-.content-body{
-    margin-top: 200px;
+body{
+    background: -webkit-linear-gradient(left, #3931af, #00c6ff);
 }
-ul {
-    list-style-type: none;
+.emp-profile{
+    padding: 3%;
+    margin-top: 3%;
+    margin-bottom: 3%;
+    border-radius: 0.5rem;
+    background: #fff;
 }
-
-a{
-  text-decoration: none;
+.profile-img{
+    text-align: center;
+}
+.profile-img img{
+    width: 70%;
+    height: 100%;
+}
+.profile-img .file {
+    position: relative;
+    overflow: hidden;
+    margin-top: -20%;
+    width: 70%;
+    border: none;
+    border-radius: 0;
+    font-size: 15px;
+    background: #212529b8;
+}
+.profile-img .file input {
+    position: absolute;
+    opacity: 0;
+    right: 0;
+    top: 0;
+}
+.profile-head h5{
+    color: #333;
+}
+.profile-head h6{
+    color: #0062cc;
+}
+.profile-edit-btn{
+    border: none;
+    border-radius: 1.5rem;
+    width: 70%;
+    padding: 2%;
+    font-weight: 600;
+    color: #6c757d;
+    cursor: pointer;
+}
+.proile-rating{
+    font-size: 12px;
+    color: #818182;
+    margin-top: 5%;
+}
+.proile-rating span{
+    color: #495057;
+    font-size: 15px;
+    font-weight: 600;
+}
+.profile-head .nav-tabs{
+    margin-bottom:5%;
+}
+.profile-head .nav-tabs .nav-link{
+    font-weight:600;
+    border: none;
+}
+.profile-head .nav-tabs .nav-link.active{
+    border: none;
+    border-bottom:2px solid #0062cc;
+}
+.profile-work{
+    padding: 14%;
+    margin-top: -15%;
+}
+.profile-work p{
+    font-size: 12px;
+    color: #818182;
+    font-weight: 600;
+    margin-top: 10%;
+}
+.profile-work a{
+    text-decoration: none;
+    color: #495057;
+    font-weight: 600;
+    font-size: 14px;
+}
+.profile-work ul{
+    list-style: none;
+}
+.profile-tab label{
+    font-weight: 600;
+}
+.profile-tab p{
+    font-weight: 600;
+    color: #0062cc;
 }
 </style>
