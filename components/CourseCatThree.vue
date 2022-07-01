517f8617791b979978717724f038804a78366fb2
Nasr Galal <nasrbue@gmail.com>
2022-07-01 12:33:29 +0200

    refactor(universities): translate courses

--- components/CourseCatThree.vue
+++ components/CourseCatThree.vue
@@ -1,111 +1,111 @@
 <template>
   <section class="course-category-three">
-      <img src="/assets/images/circle-stripe.png" class="course-category-three__circle" alt="">
-      <div class="container">
-          <div class="block-title">
-              <h2 class="block-title__title">Browse online <br>
-                  course categories</h2><!-- /.block-title__title -->
-          </div><!-- /.block-title -->
-          <div class="row">
-              <div class="col-lg-3 col-md-6 col-sm-12">
-                  <div class="course-category-three__single">
-                      <img src="/assets/images/course-category-1-4.png" class="coursesImage" alt="">
-                      <div class="course-category-three__content">
-                          <h3 class="course-category-three__title"><a href="/courses">Preparatory courses </a></h3>
-                          <!-- /.course-category-three__title -->
-                          <!-- <p class="course-category-three__text">Over 752 Courses</p> -->
-                          <!-- /.course-category-three__text -->
-                      </div><!-- /.course-category-three__content -->
-                  </div><!-- /.course-category-three__single -->
-              </div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
-              <div class="col-lg-3 col-md-6 col-sm-12">
-                  <div class="course-category-three__single">
-                      <img src="/assets/images/course-category-1-2.png" class="coursesImage" alt="">
-                      <div class="course-category-three__content">
-                          <h3 class="course-category-three__title"><a href="/courses">Bachelor programme </a></h3>
-                          <!-- /.course-category-three__title -->
-                          <!-- <p class="course-category-three__text">Over 752 Courses</p> -->
-                          <!-- /.course-category-three__text -->
-                      </div><!-- /.course-category-three__content -->
-                  </div><!-- /.course-category-three__single -->
-              </div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
-              <div class="col-lg-3 col-md-6 col-sm-12">
-                  <div class="course-category-three__single">
-                      <img src="/assets/images/course-category-1-3.png" class="coursesImage" alt="">
-                      <div class="course-category-three__content">
-                          <h3 class="course-category-three__title"><a href="/courses">Master programme</a></h3>
-                          <!-- /.course-category-three__title -->
-                          <!-- <p class="course-category-three__text">Over 752 Courses</p> -->
-                          <!-- /.course-category-three__text -->
-                      </div><!-- /.course-category-three__content -->
-                  </div><!-- /.course-category-three__single -->
-              </div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
-              <div class="col-lg-3 col-md-6 col-sm-12">
-                  <div class="course-category-three__single">
-                      <img src="/assets/images/course-category-1-5.jpg" class="coursesImage" alt="">
-                      <div class="course-category-three__content">
-                          <h3 class="course-category-three__title"><a href="/courses">PHD Programme</a></h3>
-                          <!-- /.course-category-three__title -->
-                          <!-- <p class="course-category-three__text">Over 752 Courses</p> -->
-                          <!-- /.course-category-three__text -->
-                      </div><!-- /.course-category-three__content -->
-                  </div><!-- /.course-category-three__single -->
-              </div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
-          </div>
-          <div class="row justify-content-center">
-            <div class="col-lg-3 col-md-6 col-sm-12">
-                  <div class="course-category-three__single">
-                      <img src="/assets/images/course-category-1-1.png" class="coursesImage" alt="">
-                      <div class="course-category-three__content">
-                          <h3 class="course-category-three__title"><a href="/courses">MBA</a></h3>
-                          <!-- /.course-category-three__title -->
-                          <!-- <p class="course-category-three__text">Over 752 Courses</p> -->
-                          <!-- /.course-category-three__text -->
-                      </div><!-- /.course-category-three__content -->
-                  </div><!-- /.course-category-three__single -->
-              </div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
-            <div class="col-lg-3 col-md-6 col-sm-12">
-                  <div class="course-category-three__single">
-                      <img src="/assets/images/course-category-1-6.jpg" class="coursesImage" alt="">
-                      <div class="course-category-three__content">
-                          <h3 class="course-category-three__title"><a href="/courses">specialist program</a></h3>
-                          <!-- /.course-category-three__title -->
-                          <!-- <p class="course-category-three__text">Over 752 Courses</p> -->
-                          <!-- /.course-category-three__text -->
-                      </div><!-- /.course-category-three__content -->
-                  </div><!-- /.course-category-three__single -->
-              </div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
-            <div class="col-lg-3 col-md-6 col-sm-12">
-                  <div class="course-category-three__single">
-                      <img src="/assets/images/course-category-1-7.jpg" class="coursesImage" alt="">
-                      <div class="course-category-three__content">
-                          <h3 class="course-category-three__title"><a href="/courses">Certificate</a></h3>
-                          <!-- /.course-category-three__title -->
-                          <!-- <p class="course-category-three__text">Over 752 Courses</p> -->
-                          <!-- /.course-category-three__text -->
-                      </div><!-- /.course-category-three__content -->
-                  </div><!-- /.course-category-three__single -->
-              </div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
-          </div><!-- /.row -->
-          <div class="text-center">
-              <a href="/contact" class="thm-btn course-category-three__more-link">Book a Consultation</a><!-- /.thm-btn -->
-          </div><!-- /.text-center -->
-      </div><!-- /.container -->
+    <img src="/assets/images/circle-stripe.png" class="course-category-three__circle" alt="">
+    <div class="container">
+      <div class="block-title">
+        <h2 class="block-title__title">{{ $t('university.courses.title1') }} <br>
+          {{ $t('university.courses.title2') }}</h2><!-- /.block-title__title -->
+      </div><!-- /.block-title -->
+      <div class="row">
+        <div class="col-lg-3 col-md-6 col-sm-12">
+          <div class="course-category-three__single">
+            <img src="/assets/images/course-category-1-4.png" class="coursesImage" alt="">
+            <div class="course-category-three__content">
+              <h3 class="course-category-three__title"><a href="/courses">{{ $t('university.courses.prep') }}</a></h3>
+              <!-- /.course-category-three__title -->
+              <!-- <p class="course-category-three__text">Over 752 Courses</p> -->
+              <!-- /.course-category-three__text -->
+            </div><!-- /.course-category-three__content -->
+          </div><!-- /.course-category-three__single -->
+        </div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
+        <div class="col-lg-3 col-md-6 col-sm-12">
+          <div class="course-category-three__single">
+            <img src="/assets/images/course-category-1-2.png" class="coursesImage" alt="">
+            <div class="course-category-three__content">
+              <h3 class="course-category-three__title"><a href="/courses">{{ $t('university.courses.bsc') }} </a></h3>
+              <!-- /.course-category-three__title -->
+              <!-- <p class="course-category-three__text">Over 752 Courses</p> -->
+              <!-- /.course-category-three__text -->
+            </div><!-- /.course-category-three__content -->
+          </div><!-- /.course-category-three__single -->
+        </div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
+        <div class="col-lg-3 col-md-6 col-sm-12">
+          <div class="course-category-three__single">
+            <img src="/assets/images/course-category-1-3.png" class="coursesImage" alt="">
+            <div class="course-category-three__content">
+              <h3 class="course-category-three__title"><a href="/courses">{{ $t('university.courses.msc') }}</a></h3>
+              <!-- /.course-category-three__title -->
+              <!-- <p class="course-category-three__text">Over 752 Courses</p> -->
+              <!-- /.course-category-three__text -->
+            </div><!-- /.course-category-three__content -->
+          </div><!-- /.course-category-three__single -->
+        </div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
+        <div class="col-lg-3 col-md-6 col-sm-12">
+          <div class="course-category-three__single">
+            <img src="/assets/images/course-category-1-5.jpg" class="coursesImage" alt="">
+            <div class="course-category-three__content">
+              <h3 class="course-category-three__title"><a href="/courses">{{ $t('university.courses.phd') }}</a></h3>
+              <!-- /.course-category-three__title -->
+              <!-- <p class="course-category-three__text">Over 752 Courses</p> -->
+              <!-- /.course-category-three__text -->
+            </div><!-- /.course-category-three__content -->
+          </div><!-- /.course-category-three__single -->
+        </div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-lg-3 col-md-6 col-sm-12">
+          <div class="course-category-three__single">
+            <img src="/assets/images/course-category-1-1.png" class="coursesImage" alt="">
+            <div class="course-category-three__content">
+              <h3 class="course-category-three__title"><a href="/courses">{{ $t('university.courses.mba') }}</a></h3>
+              <!-- /.course-category-three__title -->
+              <!-- <p class="course-category-three__text">Over 752 Courses</p> -->
+              <!-- /.course-category-three__text -->
+            </div><!-- /.course-category-three__content -->
+          </div><!-- /.course-category-three__single -->
+        </div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
+        <div class="col-lg-3 col-md-6 col-sm-12">
+          <div class="course-category-three__single">
+            <img src="/assets/images/course-category-1-6.jpg" class="coursesImage" alt="">
+            <div class="course-category-three__content">
+              <h3 class="course-category-three__title"><a href="/courses">{{ $t('university.courses.special') }}</a></h3>
+              <!-- /.course-category-three__title -->
+              <!-- <p class="course-category-three__text">Over 752 Courses</p> -->
+              <!-- /.course-category-three__text -->
+            </div><!-- /.course-category-three__content -->
+          </div><!-- /.course-category-three__single -->
+        </div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
+        <div class="col-lg-3 col-md-6 col-sm-12">
+          <div class="course-category-three__single">
+            <img src="/assets/images/course-category-1-7.jpg" class="coursesImage" alt="">
+            <div class="course-category-three__content">
+              <h3 class="course-category-three__title"><a href="/courses">{{ $t('university.courses.certificate') }}</a></h3>
+              <!-- /.course-category-three__title -->
+              <!-- <p class="course-category-three__text">Over 752 Courses</p> -->
+              <!-- /.course-category-three__text -->
+            </div><!-- /.course-category-three__content -->
+          </div><!-- /.course-category-three__single -->
+        </div><!-- /.col-lg-3 col-md-6 col-sm-12 -->
+      </div><!-- /.row -->
+      <div class="text-center">
+        <a href="/contact" class="thm-btn course-category-three__more-link">{{ $t('university.courses.book') }}</a><!-- /.thm-btn -->
+      </div><!-- /.text-center -->
+    </div><!-- /.container -->
   </section>
 </template>
 
 <script>
-    export default {
-    auth: false,
-        name: "CourseCatThree"
-    }
+export default {
+  name: "CourseCatThree",
+  auth: false,
+}
 </script>
 
 <style scoped>
-.coursesImage{
-        object-fit: cover;
-    width: 270px;
-    height: 304px;
-    width: 100%;
+.coursesImage {
+  object-fit: cover;
+  width: 270px;
+  height: 304px;
+  width: 100%;
 }
 </style>
