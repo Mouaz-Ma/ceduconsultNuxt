8e9a07ddc58c52b55ffd84a51bc8ae94be2dea73
Nasr Galal <nasrbue@gmail.com>
2022-07-13 14:08:10 +0200

    fix(e-learning): resolve CarouselTwo component

--- components/CourseTwo.vue
+++ components/CourseTwo.vue
@@ -120,6 +120,40 @@
             }
           ]
         }
+      },
+
+      mounted() {
+        $('.course-one__carousel').owlCarousel({
+          loop: true,
+          margin: 1,
+          nav: false,
+          dots: false,
+          autoWidth: false,
+          autoplay: true,
+          smartSpeed: 700,
+          autoplayTimeout: 4000,
+          autoplayHoverPause: true,
+          responsive: {
+            0: {
+              items: 1
+            },
+            480: {
+              items: 1
+            },
+            600: {
+              items: 1
+            },
+            991: {
+              items: 2
+            },
+            1000: {
+              items: 3
+            },
+            1200: {
+              items: 3
+            }
+          }
+        });
       }
     }
 </script>
