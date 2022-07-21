eb82b775651b5440aee9c0796a0d98c8181574dd
Nasr Galal <nasrbue@gmail.com>
2022-07-21 13:41:46 +0200

    docs: complete translating university pages

--- pages/blogs/index.vue
+++ pages/blogs/index.vue
@@ -1,88 +1,93 @@
 <template>
   <div>
-          <NavThree />
-          <section class="course-one__top-title home-one">
+    <NavThree/>
+    <section class="course-one__top-title home-one">
       <div class="container">
-          <div class="block-title mb-0 mt-5">
-              <h2 class="block-title__title">Blogs and News</h2><!-- /.block-title__title -->
-          </div><!-- /.block-title -->
+        <div class="block-title mb-0 mt-5">
+          <h2 class="block-title__title">{{ $t('university.blogs.title') }}</h2><!-- /.block-title__title -->
+        </div><!-- /.block-title -->
       </div><!-- /.container -->
       <div class="course-one__top-title__curve"></div><!-- /.course-one__top-title__curve -->
-  </section>
+    </section>
     <div class="container">
-        <div class="row">
-            <div v-for="blog in onlyUniBlogs" :key="blog.id" class="col-lg-4">
-                <div class="item">
-                    <div class="course-one__single color-1 m-2">
-                        <div class="course-one__image">
-                            <img class="uniImage" :src=blog.image.url alt="">
-                        </div><!-- /.course-one__image -->
-                        <div class="course-one__content">
-                            <nuxt-link to="#" class="course-one__category">{{blog.author.username}}</nuxt-link><!-- /.course-one__category -->
-                            <h2 class="course-one__title"><nuxt-link to="/course-details">{{blog.title}}</nuxt-link></h2>
-                            <!-- /.course-one__title -->
-                            <div class="course-one__meta">
-                                <a v-for="tag in blog.tags" :key=tag href="#"><i class="fa fa-tags"></i>{{tag}}</a>
+      <div class="row">
+        <div v-for="blog in onlyUniBlogs" :key="blog.id" class="col-lg-4">
+          <div class="item">
+            <div class="course-one__single color-1 m-2">
+              <div class="course-one__image">
+                <img class="uniImage" :src=blog.image.url alt="">
+              </div><!-- /.course-one__image -->
+              <div class="course-one__content">
+                <nuxt-link to="#" class="course-one__category">{{ blog.author.username }}</nuxt-link>
+                <!-- /.course-one__category -->
+                <h2 class="course-one__title">
+                  <nuxt-link to="/course-details">{{ blog.title }}</nuxt-link>
+                </h2>
+                <!-- /.course-one__title -->
+                <div class="course-one__meta">
+                  <a v-for="tag in blog.tags" :key=tag href="#"><i class="fa fa-tags"></i>{{ tag }}</a>
 
-                            </div><!-- /.course-one__meta -->
-                            <nuxt-link to="'/blogs/'+blog._id" class="course-one__link">See Preview</nuxt-link><!-- /.course-one__link -->
-                        </div><!-- /.course-one__content -->
-                    </div><!-- /.course-one__single -->
-            </div><!-- /.item -->
-            </div>
+                </div><!-- /.course-one__meta -->
+                <nuxt-link to="'/blogs/'+blog._id" class="course-one__link">{{ $t('university.blogs.btn') }}</nuxt-link>
+                <!-- /.course-one__link -->
+              </div><!-- /.course-one__content -->
+            </div><!-- /.course-one__single -->
+          </div><!-- /.item -->
         </div>
+      </div>
     </div><!-- /.container -->
 
-              <Footer />
+    <TheFooter/>
   </div>
 
 </template>
 <script>
-  import NavThree from "@/components/NavThree";
-  import Footer from "@/components/Footer";
-  export default {
-    auth: false,
-    components: {
-      Footer,
-      NavThree
-    },
-    head() {
-      return {
-        title: "CEDU | Blogs"
-      }
-    },
-    async asyncData({
-      $axios
-    }) {
-      try {
-        const allBlogs = $axios.get('/api/blogs')
-        const allBlogsPromise = await Promise.resolve(allBlogs)
-        const allBlogsData = allBlogsPromise.data.blogs;
-        const onlyUniBlogs = []
-        allBlogsData.forEach(blog => {
-          if(blog.section === "uni")
+import NavThree from "@/components/NavThree";
+import TheFooter from "@/components/Footer";
+
+export default {
+  auth: false,
+  components: {
+    TheFooter,
+    NavThree
+  },
+  head() {
+    return {
+      title: "CEDU | Blogs"
+    }
+  },
+  async asyncData({
+                    $axios
+                  }) {
+    try {
+      const allBlogs = $axios.get('/api/blogs')
+      const allBlogsPromise = await Promise.resolve(allBlogs)
+      const allBlogsData = allBlogsPromise.data.blogs;
+      const onlyUniBlogs = []
+      allBlogsData.forEach(blog => {
+        if (blog.section === "uni")
           onlyUniBlogs.push(blog)
-        });
-        return {
-          onlyUniBlogs
-        }
-      } catch (err) {
-        console.log(err)
-      }
-    },
-    data() {
+      });
       return {
-        allBlogsData: [],
-        onlyUniBlogs: []
+        onlyUniBlogs
       }
-    },
-  }
+    } catch (err) {
+      console.log(err)
+    }
+  },
+  data() {
+    return {
+      allBlogsData: [],
+      onlyUniBlogs: []
+    }
+  },
+}
 </script>
 
 <style scoped>
-.uniImage{
-    width: 100%;
-    height: 350px;
-    object-fit: cover;
+.uniImage {
+  width: 100%;
+  height: 350px;
+  object-fit: cover;
 }
 </style>
--- pages/course/index.vue
+++ pages/course/index.vue
@@ -1,83 +1,88 @@
 <template>
   <div>
-          <NavThree />
-          <section class="course-one__top-title home-one">
+    <NavThree/>
+    <section class="course-one__top-title home-one">
       <div class="container">
-          <div class="block-title mb-0">
-              <h2 class="block-title__title">Explore our <br>
-                  Courses</h2><!-- /.block-title__title -->
-          </div><!-- /.block-title -->
+        <div class="block-title mb-0">
+          <h2 class="block-title__title">{{ $t('university.course.title1') }}<br>
+            {{ $t('university.course.title2') }}</h2><!-- /.block-title__title -->
+        </div><!-- /.block-title -->
       </div><!-- /.container -->
       <div class="course-one__top-title__curve"></div><!-- /.course-one__top-title__curve -->
-  </section>
+    </section>
     <div class="container">
-        <div class="row">
-            <div v-for="course in allCourseData" :key="course.id" class="col-lg-4">
-                <div class="item">
-                    <div class="course-one__single color-1 m-2">
-                        <div class="course-one__image">
-                            <img class="uniImage" :src=course.image.url alt="">
-                        </div><!-- /.course-one__image -->
-                        <div class="course-one__content">
-                            <nuxt-link to="#" class="course-one__category">{{course.university.title}}</nuxt-link><!-- /.course-one__category -->
-                            <h2 class="course-one__title"><nuxt-link to="/course-details">{{course.title}}</nuxt-link></h2>
-                            <!-- /.course-one__title -->
-                            <div class="course-one__meta">
-                                <a v-for="tag in course.tags" :key=tag href="#"><i class="fa fa-tags"></i>{{tag}}</a>
+      <div class="row">
+        <div v-for="course in allCourseData" :key="course.id" class="col-lg-4">
+          <div class="item">
+            <div class="course-one__single color-1 m-2">
+              <div class="course-one__image">
+                <img class="uniImage" :src=course.image.url alt="">
+              </div><!-- /.course-one__image -->
+              <div class="course-one__content">
+                <nuxt-link to="#" class="course-one__category">{{ course.university.title }}</nuxt-link>
+                <!-- /.course-one__category -->
+                <h2 class="course-one__title">
+                  <nuxt-link to="/course-details">{{ course.title }}</nuxt-link>
+                </h2>
+                <!-- /.course-one__title -->
+                <div class="course-one__meta">
+                  <a v-for="tag in course.tags" :key=tag href="#"><i class="fa fa-tags"></i>{{ tag }}</a>
 
-                            </div><!-- /.course-one__meta -->
-                            <nuxt-link to="'/course/'+course._id" class="course-one__link">See Preview</nuxt-link><!-- /.course-one__link -->
-                        </div><!-- /.course-one__content -->
-                    </div><!-- /.course-one__single -->
-            </div><!-- /.item -->
-            </div>
+                </div><!-- /.course-one__meta -->
+                <nuxt-link to="'/course/'+course._id" class="course-one__link">{{ $t('university.course.btn') }}</nuxt-link>
+                <!-- /.course-one__link -->
+              </div><!-- /.course-one__content -->
+            </div><!-- /.course-one__single -->
+          </div><!-- /.item -->
         </div>
+      </div>
     </div><!-- /.container -->
 
-              <Footer />
+    <Footer/>
   </div>
 
 </template>
 <script>
-  import NavThree from "@/components/NavThree";
-  import Footer from "@/components/Footer";
-  export default {
-    auth: false,
-    components: {
-      Footer,
-      NavThree
-    },
-    head() {
+import NavThree from "@/components/NavThree";
+import Footer from "@/components/Footer";
+
+export default {
+  auth: false,
+  components: {
+    Footer,
+    NavThree
+  },
+  head() {
+    return {
+      title: "CEDU | Courses"
+    }
+  },
+  async asyncData({
+                    $axios
+                  }) {
+    try {
+      const allCourses = $axios.get('/api/course')
+      const allCoursePromise = await Promise.resolve(allCourses)
+      const allCourseData = allCoursePromise.data.courses
       return {
-        title: "CEDU | Courses"
+        allCourseData
       }
-    },
-    async asyncData({
-      $axios
-    }) {
-      try {
-        const allCourses = $axios.get('/api/course')
-        const allCoursePromise = await Promise.resolve(allCourses)
-        const allCourseData = allCoursePromise.data.courses
-        return {
-          allCourseData
-        }
-      } catch (err) {
-        console.log(err)
-      }
-    },
-    data() {
-      return {
-        allCourseData: [],
-      }
-    },
-  }
+    } catch (err) {
+      console.log(err)
+    }
+  },
+  data() {
+    return {
+      allCourseData: [],
+    }
+  },
+}
 </script>
 
 <style scoped>
-.uniImage{
-    width: 100%;
-    height: 350px;
-    object-fit: cover;
+.uniImage {
+  width: 100%;
+  height: 350px;
+  object-fit: cover;
 }
 </style>
