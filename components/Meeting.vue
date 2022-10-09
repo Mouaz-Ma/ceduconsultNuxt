3ddbecb7031cfa5d6ed4dede9eb4823652c061b1
HanasakiShizue <hanasaki.shizue.11@gmail.com>
2022-10-09 11:52:37 +0300

    change our articles section to learn online

--- components/Meeting.vue
+++ components/Meeting.vue
@@ -4,30 +4,27 @@
           <div class="row">
               <div class="col-lg-6">
                   <div class="meeting-one__image">
-                      <img src="/assets/images/meeting-1-1.jpg" alt="Awesome Image" />
+                      <img src="/assets/images/online-learning-1-1.jpg" alt="Awesome Image" />
                   </div><!-- /.meeting-one__image -->
               </div><!-- /.col-lg-6 -->
               <div class="col-lg-6">
                   <div class="meeting-one__content">
                       <div class="block-title text-left">
-                          <h2 class="block-title__title">Our latest news <br>
-                              & articles</h2><!-- /.block-title__title -->
+                          <h2 class="block-title__title">Learn Online</h2><!-- /.block-title__title -->
                       </div><!-- /.block-title -->
-                      <p class="meeting-one__block-text">There are many variations of passages of lorem ipsum available,
-                          but the majority have suffered alteration in some form, by injected humour, or randomised words
-                          which don't look even slightly believable.</p>
+                      <p class="meeting-one__block-text">We provide online classes to teach students and help them choose what's best for them.</p>
                       <!-- /.meeting-one__block-text -->
                       <div class="meeting-one__box-wrap">
                           <div class="meeting-one__box">
                               <div class="meeting-one__bubble">Hello, I’m here to learn English.</div>
                               <!-- /.meeting-one__bubble -->
-                              <img src="/assets/images/chat-1-1.jpg" alt="Awesome Image" />
+                              <img src="/assets/images/about-2-1.jpg" alt="Awesome Image" width="84" height="84" style="object-fit:cover" />
                               <h3 class="meeting-one__title">Rhonda Mcdermond</h3><!-- /.meeting-one__title -->
                               <p class="meeting-one__text">Student</p><!-- /.meeting-one__text -->
                           </div><!-- /.meeting-one__box -->
                           <div class="meeting-one__box">
                               <div class="meeting-one__bubble">Hello, Rhonda!</div><!-- /.meeting-one__bubble -->
-                              <img src="/assets/images/chat-1-2.jpg" alt="Awesome Image" />
+                              <img src="/assets/images/team-1-1.jpg" alt="Awesome Image" width="84" height="84" />
                               <h3 class="meeting-one__title">Karleen Pedigo</h3><!-- /.meeting-one__title -->
                               <p class="meeting-one__text">Teacher</p><!-- /.meeting-one__text -->
                           </div><!-- /.meeting-one__box -->
